2772c53a5f423dca7f97ad35f40751de3590be23
deepakduvesh <121632554+deepakduvesh@users.noreply.github.com>
2024-10-22 15:58:09 +0530

    database connected

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 import  express  from "express";
 import { config } from "dotenv";
+import { connectDB } from "./data/database.js";
 const app = express();
 config({
   path:"./config.env"
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get("/",(req,res)=>{
   res.json("working")
 })
+
+connectDB();
 
 app.listen(process.env.PORT,()=>{ 
     console.log("server is working")
